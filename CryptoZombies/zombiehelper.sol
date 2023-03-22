1c00b02c21358577cc9da13aceda8cde37f95363
BaqarAli <baqarali9889@gmail.com>
2023-03-23 01:51:12 +0500

    helper methods

--- CryptoZombies/zombiehelper.sol
+++ CryptoZombies/zombiehelper.sol
@@ -4,10 +4,14 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
+
+ 
 
   function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
@@ -21,7 +25,13 @@ contract ZombieHelper is ZombieFeeding {
 
   function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
     uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // Start here
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
     return result;
   }
 
